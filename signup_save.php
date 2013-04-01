12aa7666334dbf61eff0e2c48caa1ddd3f8a3803
philipraath <philraath@gmail.com>
2013-04-01 10:42:30 -0600

    created signup_save.php, referenced it in signup.html

---
+++ signup_save.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('lib/user_login.php');
+$user_login = new User_Login();
+
+$login_array = array();
+
+	foreach ($_POST as $key => $value) {
+		switch ($key)	{
+			case 'email':
+				$login_array['email'] = $_POST['email'];
+				break;
+			case 'password':
+				$login_array['password'] = $_POST['password'];
+				break;
+			default:
+			break;
+		}
+	}
+
+
+if($user_login::login($login_array['email'], $login_array['password']))	{
+	echo "passwords match <br/>";
+}	else 	{
+	echo "You need to activate your account before you can login. <br/>";
+}
+
+
+?>
